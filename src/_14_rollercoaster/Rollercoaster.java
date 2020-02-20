b38599957277019c487c8c29aeba10cd30b480ee
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-02-20 13:27:51 -0800

    Rollercoaster and voting  booth

---
+++ src/_14_rollercoaster/Rollercoaster.java
@@ -0,0 +1,32 @@
+package _14_rollercoaster;
+
+import javax.swing.JOptionPane;
+
+public class Rollercoaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number ;
+		number= JOptionPane.showInputDialog(null, "How tall are you in inches?");
+		 int height = Integer.parseInt(number);
+		 if(height >= 49) {
+		 JOptionPane.showMessageDialog(null, "WElcome to the hood ");
+		 }	 
+		 
+		 else { 
+			 JOptionPane.showMessageDialog(null, "Grow More First");
+		 }
+		 
+		 
+		 
+ 
+			 
+
+
+
+		 
+		
+
+	}
+
+}
---
+++ src/_15_voting_booth/Voting.java
@@ -0,0 +1,10 @@
+package _15_voting_booth;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
